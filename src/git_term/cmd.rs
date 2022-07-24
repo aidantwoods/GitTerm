a721ae73760d2f086670eff10fae9a9448a6eb77
Aidan Woods <git@public.aidanwoods.com>
2022-07-25 00:08:31 +0100

    Color only for error

--- src/git_term/cmd.rs
+++ src/git_term/cmd.rs
@@ -3,5 +3,5 @@ use derive_more::Display;
 use super::coloring::ColoredPathAndInfo;
 
 #[derive(Display, Debug)]
-#[display(fmt = r"PROMPT='%f%n:{}%f%(?.. %B%F{{red}}%?%f%b)$ '", _0)]
+#[display(fmt = r"PROMPT='%f%n:{}%f%(?.$.%B%F{{red}}$%f%b) '", _0)]
 pub struct Prompt(pub ColoredPathAndInfo);
